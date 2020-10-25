aa231533a8fbfa17d63825e92a793c7fba7301c9
Denis <crippton@gmail.com>
2020-10-25 23:35:36 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = [];
+  const openBrackets = [];
+  const closeBrackets = [];
+
+  bracketsConfig.forEach((item) => {
+    openBrackets.push(item[0]);
+    closeBrackets.push(item[1]);
+  });
+
+  for (let index = 0; index < str.length; index++) {
+    if (closeBrackets.includes(str[index]) && arr.length !== 0) {
+      const correspondBracket = openBrackets[closeBrackets.indexOf(str[index])];
+      if (arr[arr.length - 1] === correspondBracket || arr[arr.length - 1] === str[index]) {
+        arr.pop();
+
+      } else if (str[index] !== correspondBracket) {
+        return false;
+
+      } else {
+
+        arr.push(str[index]);
+      }
+    } else {
+      arr.push(str[index]);
+    }
+  }
+  return arr.length === 0;
+};
